22c024e0ceb7d8b6b9f4732bbeeab028540bf1b6
ciussky <ciussky@yahoo.ro>
2022-11-12 19:17:31 +0200

    aaa

---
+++ test-app/server/models/persons.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataType) => {
+  let model = sequelize.define('Persons', {
+    fname: {
+      type: DataType.TEXT
+    }
+  }, {
+    timestamps: true
+  });
+  /*
+    Aceasta linie este comentata pentru a demonstra legatura dintre tabelul Information si tabelul Post prin id
+  */
+  // model.belongsTo(sequelize.models.Post, {foreignKey: 'id_post', onDelete: 'set null'});
+  return model;
+};
